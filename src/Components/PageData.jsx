015ac66012073b8e71cafca4d292a40d3d7b6543
Kennytech01 <Kennysonvictory@gmail.com>
2023-08-10 19:19:26 +0100

    ready

--- src/Components/PageData.jsx
+++ src/Components/PageData.jsx
@@ -7,79 +7,147 @@ import bag from '../assets/images/bag.jpeg'
 import food2 from '../assets/images/food2.jpeg'
 import health from '../assets/images/health.jpeg'
 import drones from '../assets/images/drones.jpeg'
+import forestTree from '../assets/images/forestTree.jpeg'
+import animal from '../assets/images/animal.jpeg'
+import schoolGirl from '../assets/images/schoolGirl.jpeg'
+import friuts from '../assets/images/fruits.jpeg'
+import mountain from '../assets/images/mountain.jpeg'
+import ladyEating from '../assets/images/ladyEating.jpeg'
+import goodHealth from '../assets/images/goodHealth.jpeg'
 
 const PageData = [
     {
         image: `${nature}`,
-        type: 'nature',
+        type1: 'Nature',
         header: 'Autumn is a second spring when every leaf is a flower',
         details: 'She then expatiated very warmly upon the advantages I should reap from her plan; talked in a high style of my future grandeur; assured me how heartily I should despise almost every body and...',
-        date: 'May 2, 2023',
+        date: 'August 2, 2023',
         time: '4 min read'
     },
     {
         image: `${travel}`,
-        type: 'travel',
+        type1: 'Travel',
         header: 'Never let your memories be greater than your dreams',
         details: 'Before long the searchlight discovered some distance away a schooner with all sails set, apparently the same vessel which had been noticed earlier in the evening. The wind had by this time',
-        date: 'May 2, 2023',
+        date: 'July 30, 2023',
         time: '4 min read'
     },
     {
         image: `${food}`,
-        type: 'food',
+        type1: 'Food',
         header: 'Dramatically improve your cooking using just your imagination',
         details: 'You cannot too assiduously attend to Madame Duval herself; but I would wish you to mix as little as possible with her associates, who are not likely to be among those whose acquaintance would',
-        date: 'May 2, 2023',
+        date: 'July 28, 2023',
         time: '4 min read'
     },
     {
         image: `${lifestyle}`,
-        type: 'lifestyle',
+        type1: 'Lifestyle',
         header: 'Self-observation is the first step of inner unfolding',
         details: "Mr. Branghton's house is small and inconvenient; though his shop, which takes in all the ground floor, is large and commodious. I believe I told you before, that he is a silver-smith.",
-        date: 'May 2, 2023',
+        date: 'July 20, 2023',
         time: '5 min read'
     },
     {
         image: `${inspiration}`,
-        type: 'inspiration',
+        type1: 'Inspiration',
         header: 'It is during our darkest moments that we must focus to see the light',
         details: 'Before long the searchlight discovered some distance away a schooner with all sails set, apparently the same vessel which had been noticed earlier in the evening. The wind had by this time',
-        date: 'May 2, 2023',
+        date: 'July 19, 2023',
         time: '4 min read'
     },
     {
         image: `${bag}`,
-        type: 'bag',
+        type1: 'Technology',
+        type2: 'Nature',
+        type3: 'Inspiration',
         header: 'Bangladesh has developed plastic alternative using jute',
         details: "Dr. Johnson, as usual, spoke contemptuously of Colley Cibber. 'It is wonderful that a man, who for forty years had lived with the great and the witty, should have acquired so ill the talents of",
-        date: 'Apirl 18, 2023',
+        date: 'June 18, 2023',
         time: '3 min read'
     },
     {
         image: `${food2}`,
-        type: 'food2',
+        type1: 'Food',
         header: 'I do not stick to rules when cooking. I rely on my imagination',
         details: "I walked down to the station with them, and then wandered through the streets of the little town, finally returning to the hotel, where I lay upon the sofa and tried to interest myself in a yellow-",
-        date: 'march 31 2, 2023',
+        date: 'June 12, 2023',
         time: '4 min read'
     },
     {
         image: `${health}`,
-        type: 'health',
+        type1: 'health',
         header: 'The mind and body are not separate. what affects one, affects the other',
         details: 'His recital put the Captain into an ecstasy; he went from the lady to the gentleman, and from the gentleman to the lady, to enjoy alternately the sight of their distress. He really shouted with',
-        date: 'march 28, 2023',
+        date: 'June 05, 2023',
         time: '3 min read'
     },
     {
         image: `${drones}`,
-        type: 'drones',
+        type1: 'Technology',
         header: 'Amphibious drone are being used to send medicine to flooded area',
         details: "Another tragedy. Had single watch to-night, as crew too tired to double. When morning watch came on deck could find no one except steersman. Raised outcry, and all came on deck. Thorough",
-        date: 'March 2, 2022',
+        date: 'May 28, 2022',
         time: '4 min read'
+    },
+    {
+        image: `${forestTree}`,
+        type1: 'Nature',
+        header: 'In all things of nature there is something of the marvelous',
+        details: "Though thus elevated into fame, and conscious of uncommon powers, he had not that bustling confidence, or, I may rather say, that animated ambition, which one might have supposed would...",
+        date: 'May 20, 2022',
+        time: '3 min read'
+    },
+    {
+        image: `${animal}`,
+        type1: 'Travel',
+        header: "Don't listen to what they say, go see",
+        details: "They got up and put on their coats, sipping their coffee standing. The painter was silent and preoccupied. There was a gloom over him. He could not bear this marriage, and yet it seemed to him to",
+        date: 'May 19, 2022',
+        time: '4 min read'
+    },
+    {
+        image: `${schoolGirl}`,
+        type1: 'Travel',
+        header: "Stop worrying about the potholes in the road and enjoy the journey",
+        details: "Six o’clock struck on the bells of the church that was so conveniently near to Mr. Utterson’s dwelling, and still he was digging at the problem. Hitherto it had touched him on the...",
+        date: 'May 2, 2022',
+        time: '3 min read'
+    },
+    {
+        image: `${friuts}`,
+        type1: 'Food',
+        type2: 'Inspiration',
+        header: 'The secret ingredient in good food is always love',
+        details: "I am surely in the toils. Last night the Count asked me in the suavest tones to write three letters, one saying that my work here was nearly done, and that I should start for home within a few",
+        date: 'April 28, 2022',
+        time: '3 min read'
+    },
+    {
+        image: `${mountain}`,
+        type1: 'Nature',
+        type2: "Travel",
+        header: "With age, comes wisdom. With travel, comes understanding",
+        details: "It was a cold morning of the early spring, and we sat after breakfast on either side of a cheery fire in the old room at Baker Street. A thick fog rolled down between the lines of dun-coloured",
+        date: 'April 01, 2022',
+        time: '4 min read'
+    },
+    {
+        image: `${ladyEating}`,
+        type1: 'Food',
+        header: "I like better than talking about food is eating",
+        details: "In one point he was more fortunate than the novel's fantastic hero. He never knew--never, indeed, had any cause to know--that somewhat grotesque dread of mirrors, and polished metal",
+        date: 'March 10, 2022',
+        time: '2 min read'
+    },
+    {
+        image: `${goodHealth}`,
+        type1: 'Health',
+        type2: "Lifstyle",
+        header: "All the money in the world can't buy you back good health",
+        details: "My dear, it never rains but it pours. How true the old proverbs are. Here am I, who shall be twenty in September, and yet I never had a proposal till to-day, not a real proposal, and to-day I have had",
+        date: 'Feb, 2022',
+        time: '3 min read'
     }
 ]
 export default PageData
--- src/Pages/PageDetails.jsx
+++ src/Pages/PageDetails.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageData from '../Components/PageData'
 import {BsCalendarDate} from 'react-icons/bs'
 import {TfiTime} from 'react-icons/tfi'
+import {PiDotDuotone} from 'react-icons/pi'
+
 export const PageDetails = () => {
+  const [visible, setVisible] = useState(4)
+
+  const Showmore = ()=>{
+    set
+  }
   return (
-    <div className='py-16 mx-10'>
-      <div className=''>
+    <div className=' m-auto flex flex-cols-2 justify-center items-cente'>
+      <div className='m-10 overflow-y-scroll  scroll-smooth no-scrollbar'>
         {
-          PageData.map((pages, id) => (
-            <div key={id} className='flex w-2/3 justify-cente p-5 my-16 rounded-xl bg-stone-100 h-80 shadow-inner'>
+          PageData?.slice(0,visible).map((pages, id) => (
+            <div key={id} className='flex flex-shrink-0 w-full p-5 my-10 rounded-xl bg-stone-100  shadow-inner'>
               <div className=''>
-                <img src={pages.image} alt="image" className='rounded-xl h-full  object-cover ' />
+                <img src={pages.image} alt="image" className='rounded-xl h-full w-full  object-cover ' />
               </div>
               <div className='pl-4'>
-                <span className='font-light bg-stone-300 rounded-full p-2 px-4 capitalize '>{pages.type}</span>
-                <p className='text-3xl hover:cursor-pointer py-5 px-3 font-bold hover:underline decoration-[#EC094D] decoration-2 underline-offset-4'>{pages.header}</p>
+                <div className='list-disc flex flex-shrink-0 '>
+                  { pages.type1? (<span className='font-light bg-stone-200 rounded-full mx-2 p-2 px-3 capitalize relative flex justify-center items-center'><span className='bg-green-500 rounded-full mr-1 w-2 h-2 flex justify-center items-center  '></span>{pages.type1}</span>) : null}
+                  { pages.type2? (<span className='font-light bg-stone-200 rounded-full mx-2 p-2 px-3 capitalize relative flex justify-center items-center'><span className='bg-pink-500 rounded-full mr-1 w-2 h-2 flex justify-center items-center '></span>{pages.type2}</span>) : null}
+                  { pages.type3? (<span className='font-light bg-stone-200 rounded-full mx-2 p-2 px-3 capitalize relative flex justify-center items-center'><span className='bg-blue-600 rounded-full mr-1 w-2 h-2 flex justify-center items-center '></span>{pages.type3}</span>) : null}
+                </div>
+                <p className='py-4'><a className='text-3xl hover:cursor-pointer py-5 px-3 font-bold hover:underline decoration-[#EC094D] decoration-2 underline-offset-4'>{pages.header}</a></p>
                 <p className='tracking-wide p-3 text-stone-600 detail font-thin'>{pages.details}</p>
                 <p className="font-light text-stone-500 text-sm flex ">
                   <span className="p-2 pl-2 px-4 flex justify-center items-center"><BsCalendarDate className="m-1"/>{pages.date}</span>
@@ -24,7 +35,12 @@ export const PageDetails = () => {
             </div>
           ))
         }
+        <div className=' m-auto flex justify-center items-center p-2'>
+          <button onClick={ShowMore} className='bg-[#EC094D] text-stone-100 p-3 px-5 rounded-full hover:bg-[#e4c5ce] font-bold hover:scale-110 shadow-inner ease-in duration-200'>Load More</button>
+        </div>
       </div>
+      {/* Sidebar */}
+      <div className='w-[80%] rounded-xl bg-stone-100 shadow-inner m-10'>Helo </div>
     </div>
   )
 }
