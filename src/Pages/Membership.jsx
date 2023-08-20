3fb26f7f4c5cd1bed018e06e318911281c9e5c4d
Kennytech01 <Kennysonvictory@gmail.com>
2023-08-21 00:18:25 +0100

    ready

--- src/Pages/Membership.jsx
+++ src/Pages/Membership.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {LiaCheckCircle} from 'react-icons/lia'
+import {MdKeyboardArrowDown, MdKeyboardArrowUp} from 'react-icons/md'
 
 const data =[
     {
@@ -38,8 +39,45 @@ const data =[
         opt4: 'Simple, secure card payment',
         submit: 'Subscribe now'
     },
+    {
+        title: 'What is the Neon subscription model?',
+        details: "Pellentesque nec dictum magna, sit amet rutrum tortor. Nullam turpis ante, dictum imperdiet massa non, fermentum fringilla urna. Curabitur viverra consequat condimentum. Nulla sollicitudin eros in congue ultricies.Proin ultrices magna ac nibh imperdiet, faucibus consectetur mauris aliquet. Nam et aliquet justo, nec mollis quam. Mauris commodo mattis elit nec mollis. Vestibulum auctor posuere rhoncus."
+    },
+    {
+        title: 'What is the difference between plans?',
+        details: " Nunc luctus, ipsum nec euismod ornare, velit diam mattis mauris, sit amet euismod orci mi et lorem. Donec eu fringilla risus, ut auctor dolor. Maecenas facilisis mattis tortor a eleifend."
+    },
+    {
+        title: "How can I maximize the benefits of my subscription?",
+        details: "Duis suscipit ligula nec sollicitudin cursus. Proin eros mi, rutrum vitae luctus sed, sollicitudin nec libero. Phasellus id diam accumsan, faucibus risus sed, vestibulum purus.Fusce nec nunc malesuada, vulputate enim eu, ultrices diam. Quisque dictum fringilla iaculis. Mauris eleifend, neque id sollicitudin tempus, ipsum nunc elementum purus, sit amet interdum nunc turpis non lorem."
+    },
+    {
+        title: "How can I manage my subscription?",
+        details: "Sed non libero rhoncus, fermentum odio eu, pulvinar turpis. Proin id tempor ante, at rutrum tellus. Nunc euismod interdum justo, ut tristique justo tincidunt in. Nunc vestibulum cursus viverra."
+    },
+    {
+        title: "How to upgrade/downgrade my subscription plan?",
+        details: "Integer tincidunt vel nibh at pellentesque. Maecenas a aliquam tellus. Curabitur at ligula dictum urna venenatis tristique. Aenean sit amet ultrices ante, a ullamcorper nisl. Curabitur hendrerit purus ac est dignissim, vel elementum diam ullamcorper."
+    },
+    {
+        title: "How to renew my existing plan?",
+        details: "Integer tincidunt vel nibh at pellentesque. Maecenas a aliquam tellus. Curabitur at ligula dictum urna venenatis tristique. Aenean sit amet ultrices ante, a ullamcorper nisl. Curabitur hendrerit purus ac est dignissim, vel elementum diam ullamcorper."
+    },
+    {
+        title: "What is the cancelation policy?",
+        details: "Etiam tincidunt vitae nulla sit amet porttitor. Aenean fringilla erat purus, in varius turpis pellentesque vitae.",
+        list1: 'Ut et elit cursus, viverra lorem at, tempor erat.',
+        list2: 'Duis maximus id massa in rhoncus.',
+        list3: 'Sed aliquam condimentum porta.Integer metus urna, imperdiet in feugiat sit amet, lacinia aliquet mauris.',
+        list4: 'Etiam quis tortor augue. Nullam sit amet dignissim arcu.'
+    }
 ]
 export const Membership = () => {
+    const [dropdown, setDropDown] = useState(false)
+    const toggle = (id) => {
+        setDropDown(dropdown === id? undefined : id)
+    }
+
   return (
     <div>
         <div className='text-center'>
@@ -51,7 +89,7 @@ export const Membership = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-5 gap-8 m-2'>
                 {
-                    data.map((item)=>{
+                    data.filter((item)=> item.id).map((item)=>{
                         return (
                             <div key={item.id} className='border rounded text-stone-700 bg-white shadow-xl'>
                                 <div className='text-start border-b-2 p-5 mx-4'>
@@ -76,7 +114,29 @@ export const Membership = () => {
                     })
                 }
             </div>
-        </div>
+            {/* FAQs */}
+            <div className=' bg-white mx-40 py-4 my-10 rounded shadow-inner'>
+                <h1 className='font-bold text-3xl text-start p-2 m-10'>FAQs</h1>
+                <div>
+                    {
+                        data.filter((item)=> item.title).map((item)=> {
+                            return (
+                                <div key={item.title} className='mx-10 mb-4 border rounded' >
+                                    <h1  onClick={() => toggle(item.title)}className=' p-2 rounded text-start flex justify-between items-center'>
+                                        <span className='p-2 font-bold'>{item.title}</span>
+                                        <span>{dropdown === item.title? <MdKeyboardArrowUp/> : <MdKeyboardArrowDown/>}</span>
+                                    </h1>
+                                    {
+                                        dropdown === item.title && <p className='text-start px-3 text-stone-700'>{item.details}</p>
+                                    }
+                                </div>
+                                
+                            )
+                        })
+                    }
+                </div>
+            </div> 
+        </div>      
     </div>
   )
 }
