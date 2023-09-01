ed9edf7cfbbaf65c546463a46f7e2ec70ed9d801
Kennytech01 <Kennysonvictory@gmail.com>
2023-09-01 18:22:52 +0100

    TGIF

--- src/Components/PageDetails.jsx
+++ src/Components/PageDetails.jsx
@@ -12,6 +12,7 @@ import slider2 from '../assets/images/slider2.png'
 import slider1 from '../assets/images/slider1.png'
 import bag from '../assets/images/bag.jpeg'
 import food2 from '../assets/images/food2.jpeg'
+import ladyEating from '../assets/images/ladyEating.jpeg'
 
 
 
@@ -37,17 +38,27 @@ const Data = [
     date: 'June 18, 2023',
     time: '3 min read'
 },
-  {
-      id: 3,
-      titleTwo: 'Inspiration',
-      title: 'Travel',
-      header: 'Never let your memories be greater than your dreams',
-      details: "Before long the searchlight discovered some distance away a schooner with all sails set, apparently the same vessel which had been noticed earlier in the evening. The wind had by this time backed to the east, and there was a shudder amongst the watchers on",
-      date: "May 2, 2022",
-      time: "4 min read",
-      image: `${slider3}`
-  },
-  {
+
+    {
+      id: 5,
+      titleOne: 'Health',
+      titleTwo: 'Lifestyle',
+      header: "All the money in the world can't buy you back good health",
+      details: "My dear, it never rains but it pours. How true the old proverbs are. Here am I, who shall be twenty in September, and yet I never had a proposal till to-day, not a real proposal, and to-day I have had three. Just fancy! THREE proposals in one day! Isn't",
+      date: " Oct 9, 2021",
+      time: "3 min read",
+      image: `${slider5}`
+    },
+    {
+        id: 3,
+        image: `${ladyEating}`,
+        titleTwo: 'Food',
+        header: "I like better than talking about food is eating",
+        details: "In one point he was more fortunate than the novel's fantastic hero. He never knew--never, indeed, had any cause to know--that somewhat grotesque dread of mirrors, and polished metal",
+        date: 'March 10, 2022',
+        time: '2 min read'
+    },
+   {
       id: 4,
       titleOne: "Lifestyle",
       header: "Self-observation is the first step of inner unfolding",
@@ -76,17 +87,7 @@ const Data = [
     details: "I walked down to the station with them, and then wandered through the streets of the little town, finally returning to the hotel, where I lay upon the sofa and tried to interest myself in a yellow-",
     date: 'June 12, 2023',
     time: '4 min read'
-}, 
-  {
-      id: 5,
-      titleOne: 'Health',
-      titleTwo: 'Lifestyle',
-      header: "All the money in the world can't buy you back good health",
-      details: "My dear, it never rains but it pours. How true the old proverbs are. Here am I, who shall be twenty in September, and yet I never had a proposal till to-day, not a real proposal, and to-day I have had three. Just fancy! THREE proposals in one day! Isn't",
-      date: " Oct 9, 2021",
-      time: "3 min read",
-      image: `${slider5}`
-  },  
+}  
 ]
 export const PageDetails = ({ID}) => {
   const [visible, setVisible] = useState(4)
@@ -110,11 +111,13 @@ export const PageDetails = ({ID}) => {
         {
           Data.slice(0, visible).map((pages) => (
             <Link to={`/blog/${pages.id}`}  element={<Blog/>} key={pages.id}>
-              <div className='h-96 w-full flex md:flex-col flex-row mb-10 items-center rounded-xl shadow-inner bg-white'>
-                <div className='image relative h-1/2 w-2/3 flex justify-center '>
-                  <img src={pages.image} alt="image" className='rounded-xl object-cover w-full p-2'/>
+              <div className='h-96 w-full flex flex-col mb-10 items-center rounded-xl shadow-inner bg-white'>
+                {/* image */}
+                <div className='image relative h-1/2 md:w-2/3 flex justify-center '>
+                  <img src={pages.image} alt="image" className='rounded-xl object-cover w-full h-full p-2'/>
                 </div>
-                <div className='details p-1 sm:h-2/3 h-full w-4/5 flex-col flex justify-center '>
+                {/* details */}
+                <div className='details p-1 sm:h-2/3 h-full md:w-4/5 flex-col flex justify-center '>
                   <div className='list-disc flex flex-wrap items-start justify-start'>
                     { pages.titleOne? (<span className='font-light bg-stone-200 rounded-full my-1 mx-2 p-1 px-3 sm:text-normal text-sm capitalize relative flex justify-center items-center'><span className='bg-green-500 rounded-full mr-1 w-2 h-2 flex justify-center items-center  '></span>{pages?.titleOne}</span>) : null}
                     { pages.titleTwo? (<span className='font-light bg-stone-200 rounded-full my-1 mx-2 p-1 px-3 sm:text-normal text-sm capitalize relative flex justify-center items-center'><span className='bg-pink-500 rounded-full mr-1 w-2 h-2 flex justify-center items-center '></span>{pages?.titleTwo}</span>) : null}
@@ -122,8 +125,8 @@ export const PageDetails = ({ID}) => {
                   </div>
                   <p className=' px-2'><a className='lg:text-2xl hover:cursor-pointer px-1 font-bold hover:underline decoration-[#EC094D] decoration-2 underline-offset-4'>{pages.header}</a></p>
                   <p className='md:tracking-wide p-2 text-sm md:text-normal text-stone-600 detail '>{truncateString(pages?.details, 90)}</p>
-                  <p className="font-light text-stone-500 text-xs sm:flex  justify-start">
-                    <span className="pl-2 px-4 flex  items-center"><BsCalendarDate className="m-1"/>{pages?.date}</span>
+                  <p className="font-light text-stone-500 text-xs flex  justify-start">
+                    <span className="pl-2 px-4 flex items-center"><BsCalendarDate className="m-1"/>{pages?.date}</span>
                     <span className="px-4 flex  items-center "><TfiTime className="m-1"/>{pages?.time}</span>
                   </p>
                 </div>
